bb4559e2acdf777954556bce6f4c7c48d21b5748
OlegDsalo <oleg.d.salo@gmail.com>
2021-12-01 21:03:30 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Announcement from "./components/announcement/Announcement";
+import AddAnnouncement from "./components/addAnnouncement/AddAnnouncement";
+import Homepage from "./components/homepage/Homepage";
+import {Routes, Route, Link} from "react-router-dom";
+import SingleAnnouncement from "./components/singel-announcement/SingleAnnouncement";
+import EditAnnouncement from "./components/editAnc/EditAnnouncement";
+import Error from "./components/Page404/Error";
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <header>
+                <Link to='/'>Home </Link>
+                <Link to='/announcement'>Announcement </Link>
+                <Link to='/add'>Add announcement </Link>
+            </header>
+            <Routes>
+                <Route path='/' element={<Homepage/>}/>
+                <Route path='/announcement' element={<Announcement/>}/>
+                <Route path='/add' element={<AddAnnouncement/>}/>
+                <Route path='/announcement/:id' element={<SingleAnnouncement/>}/>
+                <Route path='/announcement/:id/edit' element={<EditAnnouncement/>}/>
+                <Route path='*' element={<Error/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
